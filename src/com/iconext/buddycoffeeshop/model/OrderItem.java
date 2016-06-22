17bf100eb19a65946bdc44556de60a32039ebbdf
wirachai <wirachai@iconext.co.th>
2016-06-22 17:52:12 +0700

    Add model classes

---
+++ src/com/iconext/buddycoffeeshop/model/OrderItem.java
@@ -0,0 +1,44 @@
+package com.iconext.buddycoffeeshop.model;
+
+public class OrderItem {
+    private Item item;
+    private int quantity;
+    private double amount;
+    private double discount;
+
+    public OrderItem(Item item) {
+        this.item = item;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+}
